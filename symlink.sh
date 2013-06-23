b6d66fef34c3ee88c135a7cb2b7d78a6e946211e
math-recognition5 <demizu@ip-10-134-139-125.ap-northeast-1.compute.internal>
2013-06-23 01:06:05 +0000

    if remote, symlink make .tmux.conf for remote

--- symlink.sh
+++ symlink.sh
@@ -11,3 +11,9 @@ done
 if [ ! -d ~/.vim/vundle.git ]; then
     git clone http://github.com/gmarik/vundle.git ~/.vim/vundle.git
 fi
+
+if [ $TMUX_ENV = "REMOTE" ]; then
+    echo "set-option -g prefix C-t" > ~~~tmp
+    cat ~~~tmp .tmux.conf > .tmux.conf
+    rm -f ~~~tmp
+fi
