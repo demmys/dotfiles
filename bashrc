ba86b81624079d66e1db727afe6ca873a5dbe6f5
Atsuki Demizu <demmys.atsuki@gmail.com>
2016-05-21 16:11:30 +0900

    Bashrcを追加

---
+++ bashrc
@@ -0,0 +1,23 @@
+is_available_command() {
+    if command -v $1 >/dev/null 2>&1
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# PATH for brewed softwares
+if is_available_command "brew"
+then
+    PATH=/usr/local/bin:/usr/local/sbin:$PATH
+fi
+# PATH for home-build softwares
+bindirs=($HOME/bin $HOME/scripts)
+for d in ${bindirs[@]}
+do
+    if [ -d "$d" ]
+    then
+        PATH=$d:$PATH
+    fi
+done
--- symlink.sh
+++ symlink.sh
@@ -1,28 +1,100 @@
 #!/bin/sh
 
+is_available_command() {
+    if command -v $1 >/dev/null 2>&1
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+add_settings_head() {
+    echo "# Settings for $2" >> $1
+    echo "if is_available_command \"$2\"" >> $1
+    echo "then" >> $1
+}
+
+add_settings_tail() {
+    echo "fi" >> $1
+}
+
+setup_bash() {
+    local tmpfile=/tmp/bashrc.tail
+    if is_available_command "pyenv"
+    then
+        add_settings_head $tmpfile "pyenv"
+        pyenv init - >> $tmpfile
+        add_settings_tail $tmpfile
+    fi
+
+    if is_available_command "rbenv"
+    then
+        add_settings_head $tmpfile "rbenv"
+        rbenv init - >> $tmpfile
+        add_settings_tail $tmpfile
+    fi
+
+    if is_available_command "direnv"
+    then
+        add_settings_head $tmpfile "direnv"
+        direnv hook bash >> $tmpfile
+        add_settings_tail $tmpfile
+    fi
+    cat bashrc $tmpfile > .bashrc
+    rm -f $tmpfile
+}
+
+setup_vim() {
+    if [ ! -d ~/.vim/bundle/neobundle.vim ]
+    then
+        mkdir -p ~/.vim/bundle
+        git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+        echo "Installed NeoBundle."
+    fi
+}
+
+setup_tmux() {
+    local tmpfile=/tmp/tmux.conf.head
+    if [ ! -z $TMUX_ENV ]
+    then
+        echo "set-option -g prefix C-t" > $tmpfile
+        echo ".tmux.conf setting for remote."
+    else
+        echo "set-option -g prefix C-g" > $tmpfile
+        echo ".tmux.conf setting for local."
+    fi
+    cat $tmpfile tmux.conf > .tmux.conf
+    rm -f $tmpfile
+}
+
+# Setup
 cd $(dirname $0)
-
-if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
-    mkdir -p ~/.vim/bundle
-    git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
-    echo "Installed NeoBundle."
-fi
-
-if [ ! -z $TMUX_ENV ]; then
-    echo "set-option -g prefix C-t" > ~~~tmp
-    echo ".tmux.conf setting for remote."
+# .bashrc
+setup_bash
+# .vimrc
+if is_available_command "vim"
+then
+    setup_vim
 else
-    echo "set-option -g prefix C-g" > ~~~tmp
-    echo ".tmux.conf setting for local."
+    echo "Vim is not installed. Settings for Vim is skipped."
 fi
-cat ~~~tmp tmux.conf > ~~~tmux.conf
-mv ~~~tmux.conf .tmux.conf
-rm -f ~~~tmp
+# .tmux.conf
+if is_available_command "tmux"
+then
+    setup_tmux
+else
+    echo "Tmux is not installed. Settings for Tmux is skipped."
+fi
 
+# Make symlinks
 dotfiles=`ls -A | grep "^\." | grep -v "^\.git"`
 for dotfile in $dotfiles
 do
     ln -Fis "$PWD/$dotfile" $HOME
 done
 
-echo "Installed dotfiles.\nYou should launch vim and install plugins next."
+echo "Installed dotfiles."
+echo "Next actions:"
+echo '* Add "source $HOME/.bashrc" to your .bash_profile'
+echo "* Launch vim and install plugins."
